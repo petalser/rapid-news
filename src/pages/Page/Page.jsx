f1ebe35187c3b487ed55ba158b22de2de12dd668
Alex Petrychuk <93341599+petalser@users.noreply.github.com>
2025-03-17 22:29:17 +0100

    Init

--- src/pages/Page/Page.jsx
+++ src/pages/Page/Page.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
-import { useRephrase } from "../../hooks/useRephrase";
 import Loading from "../../components/Loading/Loading";
 import "./Page.scss";
 
@@ -9,19 +8,9 @@ const Page = () => {
   const location = useLocation();
   const { slug } = useParams();
   const info = location.state || {};
-  const { rephrase } = useRephrase()
 
   const [data] = useState(info);
   const { title, description, content, url, image, publishedAt, source } = data;
-  const [rephrasedContent, setRephrasedContent] = useState(content)
-
-  useEffect(() => {
-    const processText = async () => {
-      const text = await rephrase(url)
-      if (text) setRephrasedContent(text)
-    }
-    processText()
-  }, [])
 
   useEffect(() => {
     if (!location.state && slug) {
@@ -32,6 +21,8 @@ const Page = () => {
 
   if (!data) return <Loading />;
 
+  const publishDate = new Date(publishedAt).toLocaleDateString("en-GB").replace(/\//g, ".");
+
   return (
     <main className="page">
       <div className="page__header">
@@ -41,16 +32,16 @@ const Page = () => {
         }
         <div className="page__info">
           <div className="page__tools">
-            <p>{publishedAt}</p>
-            <a href={source}>Read Origin</a>
+            <p>{publishDate}</p>
+            <a href={url}>Read Origin</a>
           </div>
           <h1 className="page__title">{title}</h1>
           <p className="page__desc">{description}</p>
         </div>
       </div>
-      <p className="page__text">{rephrasedContent}</p>
+      <p className="page__text">{content}</p>
+      <p>NOTE: This website uses a free news API that does not provide the full article text. To read the full text, please either view the original article (via the "Read Original" link in the top right) or consider using a different branch of the source code.</p>
     </main>
-
   );
 }
 
